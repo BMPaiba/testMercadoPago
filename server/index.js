fd062e330c5ae66df404018a29a51be5120c995a
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-02-08 21:01:33 -0300

    sasd!

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { MercadoPagoConfig, Preference } = require("mercadopago");
+const {  MercadoPagoConfig,Preference } = require("mercadopago");
+
 
 const client = new MercadoPagoConfig({
   accessToken:
@@ -21,34 +22,39 @@ server.get("/", (req, res) => {
 
 server.post("/create_preference", async (req, res) => {
   try {
-    const body = {
-      item: [
-        {
-          title: req.body.title,
-          quantity: Number(req.body.quantity),
-          unit_price: Number(req.body.price),
-          currency_id: "ARS",
-        },
-      ],
-      back_urls: {
-        success: "https://www.youtube.com/",
-        failure: "https://www.youtube.com/",
-        pending: "https://www.youtube.com/",
-      },
-      auto_return: "approved",
-    };
 
-    const preference = await Preference(client);
-    const result = await preference.create(body);
-    console.log(result);
-    res.json({
-      id: result.id,
-    });
-  } catch (error) {
-    console.log("error: ", error.message);
+      const body = {
+          items: [
+              {
+                  title: req.body.title,
+                  quantity: Number(req.body.quantity),
+                  unit_price: Number(req.body.price),
+                  currency_id: "ARS",
+              },
+          ],
+          back_urls: {
+              success: `https://www.youtube.com/watch?v=JUXxxjRECRg&ab_channel=LAMEDIAB%C3%81VARA`,
+              failure: "https://www.youtube.com/watch?v=JUXxxjRECRg&ab_channel=LAMEDIAB%C3%81VARA",
+              pending: "https://www.youtube.com/watch?v=JUXxxjRECRg&ab_channel=LAMEDIAB%C3%81VARA",
+          },
+          auto_return: "approved",
+      };
+      const preference =new Preference(client);
+      const result = await preference.create({body});
+      res.json({
+          id : result.id,
+      })
   }
+  catch (error) {
+      console.log(error);
+      res.status(500).json({
+          error: "Error al crear la preferencia :("
+      });
+  }
+
 });
 
+
 server.listen(port, () => {
-  console.log(`server listening on port ${port}`);
+  console.log(`El servidor está corriendo en el puerto ${port}`);
 });
