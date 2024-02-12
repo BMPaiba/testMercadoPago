180128b46dc261e2c7c3a852476eefb1b28ac95a
Brian Paiba <brianmanuelpaiba@outlook.es>
2024-02-12 19:33:12 -0300

    url

--- client/src/components/boliches/Compras.jsx
+++ client/src/components/boliches/Compras.jsx
@@ -40,8 +40,7 @@ export default function Compras() {
     locale: "es-AR",
   });
   const authorization = () => {
-    const redirectUri = currentUrl 
-      // "https://mercadopago-7p1q.onrender.com/mercadopago-authorization/success";
+    const redirectUri ="https://mercadopago-7p1q.onrender.com/mercadopago-authorization/success";
     const clientId = "7378685924902197";
     const state = uuidv4();
     const authorizationUrl = `https://auth.mercadopago.com/authorization?client_id=${clientId}&response_type=code&platform_id=mp&state=${state}&redirect_uri=${redirectUri}`;
